08b2068adaa66be46949f12a7e95fc14ac6a0890
vcarluer <vcarluer@gmail.com>
2012-09-05 15:03:44 +0200

    bitmap font!!!

--- src/gamers/associate/malwg/screens/Title.java
+++ src/gamers/associate/malwg/screens/Title.java
@@ -3,6 +3,7 @@ package gamers.associate.malwg.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -10,16 +11,19 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 public class Title implements Screen {
 	private SpriteBatch batch;
 	private Sprite sprite;
+	private BitmapFont font;
 	
 	public Title() {
 		this.batch = new SpriteBatch();
 		this.sprite = new Sprite(new TextureRegion(new Texture(Gdx.files.internal("data/title.png")), 0, 0, 800, 600));
 		this.sprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		this.font = new BitmapFont(Gdx.files.internal("data/ocra.fnt"), Gdx.files.internal("data/ocra.png"), false);
 	}
 	
 	public void render(float delta) {
 		this.batch.begin();
 		this.sprite.draw(this.batch);
+		this.font.draw(this.batch, "Yo man", 50, 50);
 		this.batch.end();
 	}
 
